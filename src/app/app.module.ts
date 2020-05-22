dd78997870b71452893890749ed949970abef06a
Darrenni97 <darrenni_@hotmail.com>
2020-05-21 19:51:37 -0700

    created base pages & components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,14 @@ import { RecipeService } from './services/recipe.service';
 //components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EditRecipeComponent } from './pages/edit-recipe/edit-recipe.component';
+import { NewRecipeComponent } from './pages/new-recipe/new-recipe.component';
+import { ShowRecipeComponent } from './pages/show-recipe/show-recipe.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, EditRecipeComponent, NewRecipeComponent, ShowRecipeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [RecipeService],
   bootstrap: [AppComponent],

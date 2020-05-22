5db7c07f7578b7a8857347c2b392a3d2c2a915be
Darrenni97 <darrenni_@hotmail.com>
2020-05-21 19:29:03 -0700

    created recipe service & construct for new recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//services
+import { RecipeService } from './services/recipe.service';
+
+//components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [RecipeService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/services/recipe.service.ts
@@ -0,0 +1,18 @@
+import { IIngredient } from './../interfaces/ingredient';
+import { Recipe } from './../classes/recipe';
+import { Injectable } from '@angular/core';
+import * as RecipeData from '../../data.json';
+import * as _ from 'lodash';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  private recipes: Recipe[] = [];
+
+  constructor() {
+    (<any>RecipeData).recipes.forEach((recipe) => {
+      this.recipes.push(new Recipe(recipe));
+    });
+  }
+}

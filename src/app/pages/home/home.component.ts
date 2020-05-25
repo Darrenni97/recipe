562a3a740503154167428893c22a2a821efaef15
Darrenni97 <darrenni_@hotmail.com>
2020-05-24 22:21:55 -0700

    added recipeservice dependancy to constructor

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(recipeService: RecipeService) {}
 
   ngOnInit(): void {}
 }
